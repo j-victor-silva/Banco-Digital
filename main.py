72e1906e5504ab89159ee02b2cf0b75bd68a2f5f
João Victor da Silva <jvictor200212@gmail.com>
2022-06-06 19:52:23 -0300

    Atualização 06/06/22 19:51

--- main.py
+++ main.py
@@ -8,26 +8,26 @@ from qt_core import *
 # IMPORT LOGIN WINDOW
 from Login.loginUI.login_window import Ui_MainWindow
 
-# IMPORT LOGIN CONFIG
-from Login.loginConfig.authenticator import autenticar
+# IMPORT CONNECTION
+from conexaoDB.conexao import ConexaoDB
 
 # MAIN WINDOW
-class MainWindow(QMainWindow):
+class MainWindow(QMainWindow): 
     def __init__(self) -> None:
         super().__init__()
         
         # CHAMA A CLASSE DE LOGIN
         self.ui = Ui_MainWindow()
         self.ui.setup_ui(self)
-        
-        # CHAMA A CLASSE DE CONFIGURAÇÕES DE LOGIN
-        # self.login_config = Autenticar()
+           
+        # CHAMA A CLASSE DE CONEXAO
+        self.conexao = ConexaoDB()
         
         # EXIBE A APLICAÇÃO
         self.show()
         
         # BOTÃO DE LOGIN
-        self.ui.pagina_inicial.login_btn.clicked.connect(autenticar)
+        self.ui.pagina_inicial.login_btn.clicked.connect(self.autenticar)
         
         # EXIBE A TELA DE REGISTRO
         self.ui.pagina_inicial.sign_btn.clicked.connect(self.show_registro)
@@ -51,11 +51,9 @@ class MainWindow(QMainWindow):
         self.ui.code.recuperar_btn.clicked.connect(self.show_new_pass)
         
     def show_registro(self) -> None:
-        # self.ui.pages.setMaximumSize(QSize(341, 465))
         self.ui.pages.setCurrentWidget(self.ui.registro.registro)
         
     def show_login(self) -> None:
-        # self.ui.pages.setMaximumSize(QSize(341, 385))
         self.ui.pages.setCurrentWidget(self.ui.pagina_inicial.login)
         
     def show_forgot(self) -> None:
@@ -66,6 +64,26 @@ class MainWindow(QMainWindow):
         
     def show_new_pass(self) -> None:
         self.ui.pages.setCurrentWidget(self.ui.pass_page.pass_page)
+        
+    def autenticar(self):
+        # Irá autenticar o usuário caso ele exista no banco de dados
+        index = 0
+        while True:
+            self.conexao.data('cadastro')
+            user = self.ui.pagina_inicial.line_user.text()
+            password = self.ui.pagina_inicial.line_password.text()
+
+            try:
+                if not user == self.conexao.listar[index]['user'] or not password == self.conexao.listar[index]['password']:
+                    index += 1
+
+                if user == self.conexao.listar[index]['user'] and password == self.conexao.listar[index]['password']:
+                    valido = True
+                    print('Deu certo')
+                    return
+
+            except IndexError as e:
+                valido = False
         
 if __name__ == '__main__':
     app = QApplication(sys.argv)
